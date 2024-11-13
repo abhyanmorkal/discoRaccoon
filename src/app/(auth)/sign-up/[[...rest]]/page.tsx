135496abdd70d2b2d449e91dc1f00ae6eae26662
Abhyan Morkal <61055763+abhyanmorkal@users.noreply.github.com>
2024-11-13 18:34:10 +0530

    disoRaccon

--- src/app/(auth)/sign-up/[[...rest]]/page.tsx
+++ src/app/(auth)/sign-up/[[...rest]]/page.tsx
@@ -1,10 +1,11 @@
 "use client"
+
 import { SignUp } from "@clerk/nextjs"
 
 const Page = () => {
   return (
     <div className="w-full flex-1 flex items-center justify-center">
-      <SignUp />
+      <SignUp fallbackRedirectUrl="/welcome" forceRedirectUrl="/welcome" />
     </div>
   )
 }
